e6e0e639cb4dca9a755382787a1b1a9e49715357
Mihir Talati <86654551+Mihir-Null@users.noreply.github.com>
2024-10-01 18:19:42 -0400

    Update WebscraperAStar.py

--- WebscraperAStar.py
+++ WebscraperAStar.py
@@ -59,7 +59,7 @@ def scrape(url, depth_left, path, filter_func, target, keywords):
     else:
         try:
             # Send a GET request to the URL with a timeout of 0.05 seconds
-            response = requests.fetch_with_retry(url)
+            response = requests.get(url, timeout = 0.5)
             
             # If the response status code is not 200 (OK), print an error message and stop
             if response.status_code != 200:
